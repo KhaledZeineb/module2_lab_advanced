d98f5788ed515f142743f0b797f9c82d759fd8e8
KhaledZeineb <khaledzeineb81@gmail.com>
2025-04-23 11:21:19 +0100

    correction

--- src/main/java/com/example/studentportal/StudentPortalApplication.java
+++ src/main/java/com/example/studentportal/StudentPortalApplication.java
@@ -2,12 +2,15 @@ package com.example.studentportal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+})
 public class StudentPortalApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(StudentPortalApplication.class, args);
     }
-
 }
